8ea2e55ccb0c0ca587ee43adff4e5fc6ec0dd149
marcin <s12497@pjwstk.edu.pl>
2023-10-18 21:08:18 +0200

    added new rules

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ required_temperature = ctrl.Antecedent(np.arange(18, 26, 1), 'required_temperatu
 avg_temperature_forecast = ctrl.Antecedent(np.arange(-20, 13, 1), 'avg_temperature_forecast')
 
 gas_needed = ctrl.Consequent(np.arange(0, 101, 1), 'gas_needed')
-heat_requirement = ctrl.Consequent(np.arange(0, 101, 1), 'heat_requirement')
 
 # Define membership functions for house_volume
 house_volume['small'] = fuzz.trimf(house_volume.universe, [0, 200, 400])
@@ -29,31 +28,27 @@ gas_needed['low'] = fuzz.trimf(gas_needed.universe, [0, 30, 60])
 gas_needed['medium'] = fuzz.trimf(gas_needed.universe, [40, 60, 80])
 gas_needed['high'] = fuzz.trimf(gas_needed.universe, [70, 90, 100])
 
-heat_requirement['low'] = fuzz.trimf(heat_requirement.universe, [0, 30, 60])
-heat_requirement['medium'] = fuzz.trimf(heat_requirement.universe, [40, 60, 80])
-heat_requirement['high'] = fuzz.trimf(heat_requirement.universe, [70, 90, 100])
 
 # Define rules
-rule1 = ctrl.Rule(house_volume['small'] & required_temperature['cold'] & avg_temperature_forecast['warm'], heat_requirement['high'])
-rule2 = ctrl.Rule(house_volume['medium'] & required_temperature['medium'] & avg_temperature_forecast['moderate'], heat_requirement['medium'])
-rule3 = ctrl.Rule(house_volume['large'] & required_temperature['warm'] & avg_temperature_forecast['cold'], heat_requirement['low'])
-
-# Define new rules based on avg_temperature_forecast and heat_requirement
-rule4 = ctrl.Rule(avg_temperature_forecast['cold'] & heat_requirement['high'], gas_needed['low'])
-rule5 = ctrl.Rule(avg_temperature_forecast['moderate'] & heat_requirement['medium'], gas_needed['medium'])
-rule6 = ctrl.Rule(avg_temperature_forecast['warm'] & heat_requirement['low'], gas_needed['high'])
-
-
+rule1 = ctrl.Rule(house_volume['small'] & required_temperature['cold'] & avg_temperature_forecast['warm'], gas_needed['low'])
+rule2 = ctrl.Rule(house_volume['medium'] & required_temperature['medium'] & avg_temperature_forecast['moderate'], gas_needed['medium'])
+rule3 = ctrl.Rule(house_volume['large'] & required_temperature['warm'] & avg_temperature_forecast['cold'], gas_needed['high'])
+rule4 = ctrl.Rule(house_volume['small'] & required_temperature['warm'] & avg_temperature_forecast['moderate'], gas_needed['medium'])
+rule5 = ctrl.Rule(house_volume['small'] & required_temperature['medium'] & avg_temperature_forecast['cold'], gas_needed['medium'])
+rule6 = ctrl.Rule(house_volume['medium'] & required_temperature['warm'] & avg_temperature_forecast['cold'], gas_needed['medium'])
+rule7 = ctrl.Rule(house_volume['medium'] & required_temperature['cold'] & avg_temperature_forecast['warm'], gas_needed['medium'])
+rule8 = ctrl.Rule(house_volume['large'] & required_temperature['cold'] & avg_temperature_forecast['moderate'], gas_needed['medium'])
+rule9 = ctrl.Rule(house_volume['large'] & required_temperature['medium'] & avg_temperature_forecast['warm'], gas_needed['medium'])
 # Create the Control System
-system = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6])
+system = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9])
 
 # Create a Simulation
 simulation = ctrl.ControlSystemSimulation(system)
 
 # Set Input Values
-simulation.input['house_volume'] = 800  # Example house volume in m^3
-simulation.input['required_temperature'] = 24  # Example expected temperature in °C
-simulation.input['avg_temperature_forecast'] = -12  # Example outside temperature in °C
+simulation.input['house_volume'] = 350  # Example house volume in m^3
+simulation.input['required_temperature'] = 23  # Example expected temperature in °C
+simulation.input['avg_temperature_forecast'] = -9  # Example outside temperature in °C
 
 # Perform the Simulation
 simulation.compute()
